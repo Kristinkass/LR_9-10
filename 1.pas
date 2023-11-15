68f42b1fabc12892445bf960b53830c11194cc62
Kristinkass <144677531+Kristinkass@users.noreply.github.com>
2023-11-16 02:14:36 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,17 @@
+﻿program z1_9;
+var a:array[1..10] of integer;
+i,n,pr:integer;
+begin
+writeln('Введите размер массива: ');
+readln(n);
+for i:=1 to n do
+begin
+write('a[',i,']=');
+readln(a[i]);
+end;
+pr:=1;
+for i:=1 to n do
+if a[i] mod 2=0 then pr:=pr*a[i];
+writeln('Произведение: ',pr);
+end.
+
